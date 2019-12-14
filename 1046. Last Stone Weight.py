6117735b44dc4644eec3a7af7bc2051cb38bf6f2
mitsuk-maksim <51207722+mitsuk-maksim@users.noreply.github.com>
2019-12-15 00:03:36 +0300

    Create 1046. Last Stone Weight.py

---
+++ 1046. Last Stone Weight.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/last-stone-weight/
+
+import heapq # модуль для работы с кучами
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        stones = [-1*i for i in stones]
+        while len(stones) > 1:
+            heapq.heapify(stones) # список в кучу
+            s1 = heapq.heappop(stones) # берем последний элемент
+            s2 = heapq.heappop(stones)
+            delta = s1 - s2
+            if delta != 0:
+                heapq.heappush(stones, delta) # добавление элемента (delta) в кучу (stones)
+        if stones:
+            return -1*stones[0]
+        else:
+            return 0
