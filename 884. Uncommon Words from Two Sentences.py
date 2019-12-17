3da8cb5814df5630305b55626a9b828d87ee5e0e
mitsuk-maksim <51207722+mitsuk-maksim@users.noreply.github.com>
2019-12-17 21:57:47 +0300

    Create 884. Uncommon Words from Two Sentences.py

---
+++ 884. Uncommon Words from Two Sentences.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/uncommon-words-from-two-sentences/
+
+class Solution:
+    def uncommonFromSentences(self, A: str, B: str) -> List[str]:
+        count = {}
+        for word in A.split():
+            count[word] = count.get(word, 0) + 1
+        for word in B.split():
+            count[word] = count.get(word, 0) + 1
+
+        return [word for word in count if count[word] == 1]
