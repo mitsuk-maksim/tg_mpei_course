07b6394aa22d7bf88f575a2dba0a41aa152c0f2c
mitsuk-maksim <51207722+mitsuk-maksim@users.noreply.github.com>
2019-12-22 00:31:30 +0300

    Create 811. Subdomain Visit Count.py

---
+++ 811. Subdomain Visit Count.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/subdomain-visit-count/
+
+class Solution:
+    def subdomainVisits(self, cpdomains):
+        counter = collections.Counter()
+        for cpdomain in cpdomains:
+            count, *domains = cpdomain.replace(" ",".").split(".")
+            for i in range(len(domains)):
+                counter[".".join(domains[i:])] += int(count)
+        return [" ".join((str(v), k)) for k, v in counter.items()]
