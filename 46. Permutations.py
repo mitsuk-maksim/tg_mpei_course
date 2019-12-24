18ee71e5652e26984f8319635fdb05ad0098d5d5
mitsuk-maksim <51207722+mitsuk-maksim@users.noreply.github.com>
2019-12-24 23:54:07 +0300

    Create 46. Permutations.py

---
+++ 46. Permutations.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/permutations/
+
+import itertools
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        return list(itertools.permutations(nums))
