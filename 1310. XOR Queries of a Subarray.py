98220698e9e7d4749ea64c2396404c4eff35a936
mitsuk-maksim <51207722+mitsuk-maksim@users.noreply.github.com>
2020-01-13 21:20:46 +0300

    Create 1310. XOR Queries of a Subarray.py

---
+++ 1310. XOR Queries of a Subarray.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/xor-queries-of-a-subarray/
+
+class Solution:
+    def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+        for i in range(len(arr) - 1):
+            arr[i+1] ^= arr[i]
+        return [arr[j]^arr[i-1] if i else arr[j] for i,j in queries]
