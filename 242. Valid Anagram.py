8f50fc0b5ac803718f137ec2d86c693620bbc319
mitsuk-maksim <51207722+mitsuk-maksim@users.noreply.github.com>
2020-01-17 18:21:22 +0300

    Create 242. Valid Anagram.py

---
+++ 242. Valid Anagram.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/valid-anagram/
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        return sorted(s) == sorted(t)
